bb0f9993bcd8dd4ba9a5203275af1bb47e56595d
Krittamett <6010110012@psu.ac.th>
2020-02-08 20:51:15 +0000

    first

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(title__contains='post').order_by('published_date')
+    posts = Post.objects.filter(title__contains='Post').order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
